d01926dac38c40df446d82267b6e2a5046d75efb
Tamas <tvirag@gmail.com>
2017-09-23 21:36:49 -0600

    Myapp original sample

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 9, name: 'Steve     ', age: 23 },
+  { id: 10, name: 'Jason     ', age: 23 },
+  { id: 11, name: 'Paul      ', age: 23 },
+  { id: 12, name: 'Narco     ', age: 23 },
+  { id: 13, name: 'Bombasto  ', age: 23 },
+  { id: 14, name: 'Celeritas ', age: 23 },
+  { id: 15, name: 'Magneta   ', age: 23 },
+  { id: 16, name: 'RubberMan ', age: 23 },
+  { id: 17, name: 'Dynama    ', age: 23 },
+  { id: 18, name: 'Dr IQ     ', age: 23 },
+  { id: 19, name: 'Magma     ', age: 23 },
+  { id: 20, name: 'Tornado   ', age: 23 }
+];
